c2258b5f04252df24f08fa96db9172ff8d67bdd9
Kavindu Rukshan <kavindurukshan27@gmail.com>
2021-05-09 19:21:31 +0530

    adds code base

---
+++ ASP.NET Core Excersise1/Controllers/UserLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Excersise1.Controllers
+{
+    public class UserLogin : Controller
+    {
+       // [HttpPost]
+        public IActionResult Index()
+        {
+          
+               return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Index(Index formdata)
+        //{
+        //       if (!ModelState.IsValid)
+        //    {
+        //        return View(formdata);
+        //    }
+        //    return RedirectToAction("Index");
+        }
+    }
+
---
+++ ASP.NET Core Excersise1/Models/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using FormFactory.Attributes;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = FormFactory.Attributes.RequiredAttribute;
+using StringLengthAttribute = FormFactory.Attributes.StringLengthAttribute;
+
+namespace ASP.NET_Core_Excersise1.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [Password]
+        public string Password { get; set; }
+    }
+}
